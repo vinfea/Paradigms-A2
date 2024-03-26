74e18bc7fe3790ac1d24f1ec32c8f1afbdc1937b
Christa Huang <christahuang46@gmail.com>
2024-03-26 15:31:10 -0400

    added player move behaviour per round

--- Q1.go
+++ Q1.go
@@ -6,16 +6,108 @@ import (
 	"time"
 )
 
-func movePolice(result chan int, pos chan int) {
-	fmt.Println("In Police Go Routine.")
+func movePolice(result chan int, pos chan int, n int, m int) {
+	var x, y, move int
+	x = 1
+	y = n
+
+	fmt.Printf("Police: (%d, %d)\n", x, y)
+
+	// using for loop for the time being
+	// before end-game logic is implemented
+	for i := 0; i < 10; i++ {
+		move = rand.Intn(4)
+
+		if move == 0 { // north
+			if (y + 1) <= n {
+				y = y + 1
+			} else {
+				y = y - 1
+			}
+		} else if move == 1 { // east
+			if (x + 1) <= m {
+				x = x + 1
+			} else {
+				x = x - 1
+			}
+		} else if move == 2 { // south
+			if (y - 1) >= 1 {
+				y = y - 1
+			} else {
+				y = y + 1
+			}
+		} else { // west
+			if (x - 1) >= 1 {
+				x = x - 1
+			} else {
+				x = x + 1
+			}
+		}
+
+		pos <- x
+		pos <- y
+	}
 }
 
-func moveThief(result chan int, pos chan int) {
-	fmt.Println("In Thief Go Routine.")
+func moveThief(result chan int, pos chan int, n int, m int) {
+	var x, y, move int
+	x = m
+	y = 1
+
+	fmt.Printf("Thief: (%d, %d)\n", x, y)
+
+	// using for loop for the time being
+	// before end-game logic is implemented
+	for i := 0; i < 10; i++ {
+		move = rand.Intn(4)
+
+		if move == 0 { // north
+			if (y + 1) <= n {
+				y = y + 1
+			} else {
+				y = y - 1
+			}
+		} else if move == 1 { // east
+			if (x + 1) <= m {
+				x = x + 1
+			} else {
+				x = x - 1
+			}
+		} else if move == 2 { // south
+			if (y - 1) >= 1 {
+				y = y - 1
+			} else {
+				y = y + 1
+			}
+		} else { // west
+			if (x - 1) >= 1 {
+				x = x - 1
+			} else {
+				x = x + 1
+			}
+		}
+
+		pos <- x
+		pos <- y
+	}
 }
 
 func controller(result1 chan int, pos1 chan int, result2 chan int, pos2 chan int, n int, m int, s int) {
-	fmt.Println("In Controller Go Routine.")
+	var x1, y1, x2, y2 int
+
+	// using for loop for the time being
+	// before end-game logic is implemented
+	for i := 0; i < 10; i++ {
+		x1 = <-pos1
+		y1 = <-pos1
+
+		x2 = <-pos2
+		y2 = <-pos2
+
+		fmt.Printf("\n---Round %d---\n", i)
+		fmt.Printf("Police at position (%d, %d).\n", x1, y1)
+		fmt.Printf("Thief at position (%d, %d).\n", x2, y2)
+	}
 }
 
 func main() {
@@ -29,16 +121,19 @@ func main() {
 
 	result1 := make(chan int)
 	result2 := make(chan int)
-	pos1 := make(chan int)
-	pos2 := make(chan int)
+	pos1 := make(chan int, 2)
+	pos2 := make(chan int, 2)
 
-	fmt.Printf("The 黑猫警长 Game begins with a %d x %d grid.\n", n, m)
+	fmt.Printf("The 黑猫警长 Game begins with a %d x %d grid.\n", m, n)
 	fmt.Printf("The number of moves for the Police to catch the Thief is %d.\n", s)
+	fmt.Println()
 
-	go movePolice(result1, pos1)
-	go moveThief(result2, pos2)
+	go movePolice(result1, pos1, n, m)
+	go moveThief(result2, pos2, n, m)
 	go controller(result1, pos1, result2, pos2, n, m, s)
 
 	time.Sleep(2 * time.Second)
+
+	fmt.Println()
 	fmt.Println("The 黑猫警长 Game is finished!")
 }
